388f4ec78a139231628563c85b85288ef1407b27
monzag <zaganiacz.m@gmail.com>
2017-10-30 10:26:01 +0100

    Add App

---
+++ src/main/java/com/monzag/checkpoint4/App.java
@@ -0,0 +1,9 @@
+package com.monzag.checkpoint4;
+
+public class App 
+{
+    public static void main( String[] args ) {
+        DBsearch dbSearch = new DBsearch();
+        dbSearch.run();
+    }
+}

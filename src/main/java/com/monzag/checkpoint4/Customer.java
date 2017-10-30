7988772a6820bfe185c406df557b034eb0232318
monzag <zaganiacz.m@gmail.com>
2017-10-30 11:10:30 +0100

    Add toString method

--- src/main/java/com/monzag/checkpoint4/Customer.java
+++ src/main/java/com/monzag/checkpoint4/Customer.java
@@ -8,11 +8,22 @@ public class Customer {
     Integer birthyear;
     String gender;
 
+
     public Customer(Integer id, String name, String surname, Integer birthyear, String gender) {
         this.id = id;
         this.name = name;
         this.surname = surname;
         this.birthyear = birthyear;
         this.gender = gender;
+    }
+
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", birthyear=" + birthyear +
+                ", gender='" + gender + '\'' +
+                '}';
     }
 }

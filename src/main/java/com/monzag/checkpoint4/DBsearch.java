e202d985402407b14f175371d597c20e16a334e9
monzag <zaganiacz.m@gmail.com>
2017-10-30 10:31:35 +0100

    Add empty constructor, methods run and getPhrase

--- src/main/java/com/monzag/checkpoint4/DBsearch.java
+++ src/main/java/com/monzag/checkpoint4/DBsearch.java
@@ -1,4 +1,23 @@
 package com.monzag.checkpoint4;
 
+import java.util.Scanner;
+
 public class DBsearch {
+
+    public DBsearch() {
+
+    }
+
+    public void run() {
+        String phrase = getPhrase();
+
+    }
+
+    public String getPhrase() {
+        String userInput;
+
+        System.out.println("\nSearch phrase: ");
+        userInput = new Scanner(System.in).nextLine();
+        return userInput;
+    }
 }

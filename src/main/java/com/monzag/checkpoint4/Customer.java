e4733dd6c07b8789d24e42bc8a80c11892273496
monzag <zaganiacz.m@gmail.com>
2017-10-30 11:03:59 +0100

    Add class Customer

---
+++ src/main/java/com/monzag/checkpoint4/Customer.java
@@ -0,0 +1,18 @@
+package com.monzag.checkpoint4;
+
+public class Customer {
+
+    Integer id;
+    String name;
+    String surname;
+    Integer birthyear;
+    String gender;
+
+    public Customer(Integer id, String name, String surname, Integer birthyear, String gender) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.birthyear = birthyear;
+        this.gender = gender;
+    }
+}
